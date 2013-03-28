4c5c401b2c29774fec47d3ce43386bf221a12cbc
Deus Duke <deus@deusenterprises.com>
2013-03-28 00:04:03 -0500

    fixed issues with default date and id

--- js/json.js
+++ js/json.js
@@ -2,21 +2,21 @@ var json = {
 	"project1": {
 		"id": "9827398723498",
 		"name": "Cool Game",
-		"startDate": '03/25/2013',
+		"startDate": '2013-03-25',
 		"type": "internal",
 		"priority": "5"
 	},
 	"project2": {
-		"id": "09237023570",
+		"id": "9237023570",
 		"name": "Awesome Idea",
-		"startDate": '06/16/2013',
+		"startDate": '2013-06-16',
 		"type": "personal",
 		"priority": "2"
 	},
 	"project3": {
 		"id": "84720597435087",
 		"name": "Amazing Application",
-		"startDate": '08/19/2013',
+		"startDate": '2013-08-19',
 		"type": "external",
 		"priority": "3"
 	}
