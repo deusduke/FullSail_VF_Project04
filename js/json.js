98385c0dcabf466f9c582d0fcf324f134a8f9a8e
Deus Duke <deus@deusenterprises.com>
2013-03-27 21:44:01 -0500

    implemented json data

---
+++ js/json.js
@@ -0,0 +1,20 @@
+var json = {
+	"project1": {
+		"name": "Cool Game",
+		"startDate": '03/25/2013',
+		"type": "internal",
+		"priority": "5"
+	},
+	"project2": {
+		"name": "Cool Game",
+		"startDate": '03/25/2013',
+		"type": "personal",
+		"priority": "2"
+	},
+	"project3": {
+		"name": "Cool Game",
+		"startDate": '03/25/2013',
+		"type": "external",
+		"priority": "3"
+	}
+};
